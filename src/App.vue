ffb7b829255318a26c6e29aaff255b745fff172b
VickyMastro <vicmastroianni1702@gmail.com>
2022-03-15 15:37:15 -0300

    Elimino lo que viene con el create de vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- <router-link to="/">H</router-link> -->
     </div>
     <router-view/>
   </div>
@@ -17,9 +16,9 @@
   color: #2c3e50;
 }
 
-#nav {
+/* #nav {
   padding: 30px;
-}
+} */
 
 #nav a {
   font-weight: bold;
