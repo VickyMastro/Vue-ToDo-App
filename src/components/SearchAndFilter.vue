b4543633e0cc0e22bf51f908186f6f47e89d5161
VickyMastro <vicmastroianni1702@gmail.com>
2022-03-17 10:42:17 -0300

    Agrego estilos a los botones y el icono de filtro

--- src/components/SearchAndFilter.vue
+++ src/components/SearchAndFilter.vue
@@ -1,22 +1,28 @@
 // filtros: prioridad, fecha, completo o incompleto
 <template>
   <div>
-    <h2 class="text-center pt-3">Notas</h2>
+    <h2 class="text-center pt-3 fs-1" style="text-decoration: underline;">
+      Notas
+    </h2>
     <div class="row">
       <div class="col-sm-12 col-md-8 align-center d-flex align-items-center">
         <input
           type="text"
-          class="search-input mx-4"
-          v-model="texto"
+          class="search-input mx-4 "
+          v-model="text"
           placeholder="Buscar nota"
         />
-        <input type="button" value="+ filtros" />
+
+        <button class="btn-grad">
+          <img src="../assets/filter.png" alt="" width="20px" height="20px" />
+        </button>
+        <!-- <input type="button" value="+ filtros" class="btn-grad"/> -->
       </div>
 
       <div class="col-sm-12 col-md-4" v-if="!isMobile">
-        <button class="d-flex align-items-center rounded-pill  ">
-          <router-link to="/notes" class="create-button ">
-            Nota Nueva 
+        <button class="d-flex btn-grad">
+          <router-link :to="{name: 'Notes'}" class="create-button">
+            Nota Nueva
           </router-link>
         </button>
       </div>
@@ -34,7 +40,7 @@ export default {
   },
   data() {
     return {
-      texto: "",
+      text: "",
       isMobile: false,
     };
   },
@@ -42,12 +48,41 @@ export default {
 </script>
 
 <style scoped>
+
 .search-input {
   width: 200px;
+  padding: 12px 30px;
+
+  background: inherit;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+
+  border: none;
+  outline: none;
+  border-radius: 10px;
 }
+
+.search-input::placeholder {
+  color: rgba(0, 0, 0, 0.568);
+}
+
 .create-button {
-  color: black;
-  font-size: 20px;
+  font-size: 15px;
   text-decoration: none;
+  color: rgba(0, 0, 0, 0.568);
+}
+
+.btn-grad {
+  margin: 10px;
+  padding: 12px 30px;
+
+  text-align: center;
+  text-transform: uppercase;
+
+  background: inherit;
+  background-size: 200% auto;
+
+  border: none;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 }
 </style>
