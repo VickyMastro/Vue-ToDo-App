99ab03f7244acf2877f3a6933909f79ce36bc1fa
VickyMastro <vicmastroianni1702@gmail.com>
2022-03-17 10:40:59 -0300

    Agrego color de fondo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <!-- <router-link to="/">H</router-link> -->
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -12,19 +11,14 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-/* #nav {
-  padding: 30px;
-} */
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  color: rgba(0, 0, 0, 0.568);
+  background-color: #4158d0;
+  background-image: linear-gradient(
+    43deg,
+    #4158d0 0%,
+    #c850c0 46%,
+    #ffcc70 100%
+  );
+  height: 100vh;
 }
 </style>
