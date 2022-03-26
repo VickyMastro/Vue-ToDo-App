a1c7068c33d3d762dcc42b48070d3bbc5e31ee50
VickyMastro <vicmastroianni1702@gmail.com>
2022-03-25 21:42:55 -0300

    creacion de local storage para notas y un estado

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,50 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   state: {
+    // props que voy a compartir
+    allNotes: []
   },
+
+  getters:{
+    // funciones que devuelven valor/es del estado
+    doneNotes(state) {
+        return state.allNotes;
+    },
+  },
+
   mutations: {
+    // func que modifican el estado
+    setNote(state, note){
+      state.allNotes.push(note)
+    },
+
+    setNotes(state, notes){
+      state.allNotes = notes
+    }
   },
+
   actions: {
+    // funcion que hace algo antes de hacer una mutación
+    addNote(context, note){
+      context.commit('setNote', note)
+
+      const newNotes= JSON.stringify(context.state.allNotes)
+      window.localStorage.setItem('notes', newNotes)
+
+    },
+    
+    passNotes(context){
+      const notes= JSON.parse(window.localStorage.getItem('notes'))
+
+      context.commit('setNotes', notes)
+    }
   },
+
   modules: {
   }
 })
+
+export default store;
+store.dispatch('passNotes')
