b7724fc7ed19b2f2092c0b776f7fa453c38b0fea
VickyMastro <vicmastroianni1702@gmail.com>
2022-03-25 21:46:07 -0300

    Agrego estilos y boton que abre los modales

--- src/components/SearchAndFilter.vue
+++ src/components/SearchAndFilter.vue
@@ -1,14 +1,21 @@
 // filtros: prioridad, fecha, completo o incompleto
 <template>
   <div>
-    <h2 class="text-center pt-3 fs-1" style="text-decoration: underline;">
+    <h2 class="text-center pt-3 fs-1" style="text-decoration: underline">
       Notas
     </h2>
     <div class="row">
-      <div class="col-sm-12 col-md-8 align-center d-flex align-items-center">
+      <div
+        class="
+          col-sm-12 col-md-6
+          d-flex
+          align-items-center
+          justify-content-center
+        "
+      >
         <input
           type="text"
-          class="search-input mx-4 "
+          class="search-input mx-4"
           v-model="text"
           placeholder="Buscar nota"
         />
@@ -16,21 +23,28 @@
         <button class="btn-grad">
           <img src="../assets/filter.png" alt="" width="20px" height="20px" />
         </button>
-        <!-- <input type="button" value="+ filtros" class="btn-grad"/> -->
       </div>
-
-      <div class="col-sm-12 col-md-4" v-if="!isMobile">
-        <button class="d-flex btn-grad">
-          <router-link :to="{name: 'Notes'}" class="create-button">
-            Nota Nueva
-          </router-link>
-        </button>
+      <!-- v-if="!isMobile" ESTO VA BAJO ESTA CLASE -->
+      <div class="col-sm-12 col-md-4 d-flex justify-content-center">
+        <select
+          class="form-select d-flex btn-grad"
+          v-model="optionSelect"
+          aria-label="Default select example"
+        >
+          <option hidden value="">Nueva/o</option>
+          <option value="nota">Nota</option>
+          <option value="toDo">ToDo</option>
+        </select>
       </div>
+      <pre> {{$data}} </pre>
     </div>
   </div>
 </template>
 
 <script>
+import ModalNote from "@/components/ModalNote.vue";
+import ModalToDo from "@/components/ModalToDo.vue";
+
 export default {
   name: "SearchAndFilter",
   mounted() {
@@ -42,13 +56,41 @@ export default {
     return {
       text: "",
       isMobile: false,
+      optionSelect: "",
     };
+  },
+  watch: {
+    optionSelect(newValue) {
+      if (newValue === "nota") {
+        this.openModalNota();
+      }
+      if (newValue === "toDo") {
+        this.openModalToDo();
+      }
+      this.optionSelect = "";
+    },
+  },
+  methods: {
+    openModalNota() {
+      this.$modal.show(ModalNote, null, {
+        adaptive: true,
+        width:"90%",
+        maxWidth: 776,
+        height: "80%",
+      });
+    },
+    openModalToDo() {
+      this.$modal.show(ModalToDo, null, {
+        adaptive: true,
+        width: "90%",
+        height: "90%",
+      });
+    },
   },
 };
 </script>
 
 <style scoped>
-
 .search-input {
   width: 200px;
   padding: 12px 30px;
@@ -65,18 +107,11 @@ export default {
   color: rgba(0, 0, 0, 0.568);
 }
 
-.create-button {
-  font-size: 15px;
-  text-decoration: none;
-  color: rgba(0, 0, 0, 0.568);
-}
-
 .btn-grad {
   margin: 10px;
   padding: 12px 30px;
 
   text-align: center;
-  text-transform: uppercase;
 
   background: inherit;
   background-size: 200% auto;
