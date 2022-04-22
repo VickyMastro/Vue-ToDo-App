5354852dc12cf108adb74819a6a43747062fec18
VickyMastro <vicmastroianni1702@gmail.com>
2022-04-22 17:47:47 -0300

    Formuilario para crear un toDo

---
+++ src/components/todos/CreateToDoModal.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="modal-container">
+    <ToDoForm :formData="formData" @setValue="setValue">
+      <div class="d-flex justify-content-around">
+        <button class="btn-grad" type="button" @click="cancel">Cancelar</button>
+
+        <button class="btn-grad" type="button" @click="save">Guardar</button>
+      </div>
+    </ToDoForm>
+  </div>
+</template>
+
+<script>
+import ToDoForm from "./ToDoForm.vue";
+
+export default {
+  name: "CreateToDoModal",
+  mounted() {
+    this.formData.date = new Date();
+  },
+  data() {
+    return {
+      formData: {
+        desc: "",
+        date: "",
+      },
+    };
+  },
+  methods: {
+    cancel() {
+      this.$modal.hideAll();
+    },
+    
+    save() {
+      // if (!this.formData.desc || !this.formData.title) {
+      //   alert("Completa todos los campos");
+
+      // } else {
+      //   this.$store.dispatch("addNote", {
+      //     desc: this.formData.desc,
+      //     date: this.formData.date,
+      //   });
+      //   this.$modal.hideAll();
+      // }
+    },
+
+    setValue(name, value) {
+      this.formData[name] = value;
+    },
+ },
+  components: {
+    ToDoForm,
+  },
+};
+</script>
+
+<style scoped>
+.modal-container{
+  height: 100%;
+  background: linear-gradient(43deg, #c850c0 20%, #ffcc70 80%);
+
+}
+</style>
