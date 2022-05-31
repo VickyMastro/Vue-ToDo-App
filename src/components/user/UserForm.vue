fab4623591525dc52858e30e8fc70f668cb0e0c8
VickyMastro <vicmastroianni1702@gmail.com>
2022-05-30 23:01:50 -0300

    creo form para los usuarios

--- src/components/user/UserForm.vue
+++ src/components/user/UserForm.vue
@@ -1,31 +1,62 @@
 <template>
-  <div>
-    <div class="form-floating mb-3">
+  <div class="user-form-container d-flex flex-column">
+    <div class="text-center fs-1">
+      <p>{{userData.titleForm}}</p>
+    </div>
+    <div class="inputs-container form-floating mb-3" v-if="isCreateUser">
+      <input
+        type="text"
+        class="form-control"
+        id="floatingName"
+        placeholder="ej.:Vi"
+        @input="$emit('setValue', 'name', $event.target.value)"
+      />
+      <label for="floatingName">Name</label>
+    </div>
+    <div class="inputs-container form-floating mb-3">
       <input
         type="email"
         class="form-control"
         id="floatingInput"
         placeholder="name@example.com"
+        @input="$emit('setValue', 'email', $event.target.value)"
       />
-      <label for="floatingInput">Email address</label>
+      <label for="floatingInput">Email</label>
     </div>
-    <div class="form-floating">
+    <div class="inputs-container form-floating">
       <input
         type="password"
         class="form-control"
         id="floatingPassword"
         placeholder="Password"
+        @input="$emit('setValue', 'password', $event.target.value)"
       />
-      <label for="floatingPassword">Password</label>
+      <label for="floatingPassword">Contraseña</label>
+      <a :href="url">{{userData.msj}}</a>
     </div>
+    <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
   name: "UserForm",
+  props: ['userData', 'isCreateUser'],
+  computed:{
+    url(){
+      return this.isCreateUser ? './LoginUser': './CreateUser'
+    },
+  }
 };
 </script>
 
-<style>
+<style scoped>
+.user-form-container{
+  max-width: 500px;
+  height: 300px;
+}
+
+.inputs-container{
+  width: 400px;
+}
 </style>
