1f0ae4ae18b5220d31e08faa4cb4105ad96ac149
VickyMastro <vicmastroianni1702@gmail.com>
2022-06-20 20:09:58 -0300

    button component for edit info from user

---
+++ src/components/buttons/EditUserButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button>
+    <img
+      src="../../assets/edit.png"
+      alt="boton de editar"
+      width="30px"
+      height="30px"
+      @click="$emit('editInfo', identifier)"
+    />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "EditUserButton",
+  props:['identifier']
+};
+</script>
+
+<style scoped>
+button{
+  width: 70px; 
+  background: inherit; 
+  border: none;
+  padding-bottom: 5px;
+}
+</style>
