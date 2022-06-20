4c3b75ce29bb50d6fb8af428b53d8a3e41dc7fb6
VickyMastro <vicmastroianni1702@gmail.com>
2022-06-20 20:29:53 -0300

    add link to home in header

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
     <nav class="container-nav navbar navbar-light">
-      <div class="container-fluid justify-content-end">
-        <router-link class="navbar-brand" :to="{ name: 'EditUser' }">
+      <div class="container-fluid justify-content-between">
+        <router-link class="navbar-brand link-router" :to="{ name: 'Home' }">
+          Home
+          <img
+            src="@/assets/home.png"
+            alt="Icono de usuario"
+            width="25"
+            height="25"
+            class="d-inline-block align-text-top"
+          />
+        </router-link>
+        <router-link class="navbar-brand link-router" :to="{ name: 'EditUser' }">
           User
           <img
             src="@/assets/user.png"
@@ -24,15 +34,12 @@ export default {
 </script>
 
 <style scoped>
-a {
-  color: black;
+.link-router {
+  color: rgba(0, 0, 0, 0.568) !important;
   text-decoration: none;
 }
 .container-nav {
   background-color: inherit;
   box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-}
-.link:hover {
-  color: black;
 }
 </style>
